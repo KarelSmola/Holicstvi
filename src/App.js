bc9a91fac991f3999f7b90b9b4781c6812fcee70
Karel Smola <karelsmola21@gmail.com>
2023-04-12 15:54:06 +0200

    Pricelist added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navigation from "./components/Navigation";
 import Hero from "./components/Hero";
+import Pricelist from "./components/Pricelist";
 
 const App = () => {
   return (
     <main>
       <Navigation />
       <Hero />
+      <Pricelist />
     </main>
   );
 };
--- src/components/Pricelist.js
+++ src/components/Pricelist.js
@@ -3,7 +3,64 @@ import React from "react";
 import classes from "./Pricelist.module.css";
 
 const Pricelist = () => {
-  return <div className={classes.pricelist}></div>;
+  return (
+    <div className={classes.pricelist}>
+      <h1 className={classes.title}>Ceník</h1>
+      <div className={classes["service-box"]}>
+        <h2 className={classes["service-title"]}>Střih vlasů</h2>
+        <p className={classes["service-text"]}>
+          Střih vlasů, zaholení kontur, úprava obočí, mytí vlasů, masáž hlavy,
+          styling
+        </p>
+        <p className={classes["service-time"]}>45 - 60 min</p>
+        <p className={classes["service-price"]}>610 Kč</p>
+      </div>
+
+      <div className={classes["service-box"]}>
+        <h2 className={classes["service-title"]}>
+          Střih vlasů + holení / úprava vousů
+        </h2>
+        <p className={classes["service-text"]}>
+          Střih vlasů, zaholení kontur, úprava obočí, mytí vlasů, masáž hlavy,
+          styling + holení dohladka HOT TOWEL nebo zkrácení vousů se zaholením,
+          ošetření pokožky po holení
+        </p>
+        <p className={classes["service-time"]}>60 min</p>
+        <p className={classes["service-price"]}>940 Kč</p>
+      </div>
+
+      <div className={classes["service-box"]}>
+        <h2 className={classes["service-title"]}>Střih vlasů strojkem</h2>
+        <p className={classes["service-text"]}>
+          Střih vlasů pouze strojkem, zaholení kontur, úprava obočí, mytí vlasů,
+          masáž hlavy, styling
+        </p>
+        <p className={classes["service-time"]}>30 min</p>
+        <p className={classes["service-price"]}>400 Kč</p>
+      </div>
+
+      <div className={classes["service-box"]}>
+        <h2 className={classes["service-title"]}>
+          Chlapecký střih vlasů (7 - 12 let)
+        </h2>
+        <p className={classes["service-text"]}>
+          Střih vlasů, mytí vlasů, styling
+        </p>
+        <p className={classes["service-time"]}>30 min</p>
+        <p className={classes["service-price"]}>500 Kč</p>
+      </div>
+
+      <div className={classes["service-box"]}>
+        <h2 className={classes["service-title"]}>Holení / úprava vousů</h2>
+        <p className={classes["service-text"]}>
+          Holení dohladka HOT TOWEL nebo zkrácení vousů se zaholením, ošetření
+          pokožky po holení
+        </p>
+        <p className={classes["service-time"]}>30 min</p>
+        <p className={classes["service-price"]}>450 Kč</p>
+      </div>
+    </div>
+  );
 };
 
 export default Pricelist;

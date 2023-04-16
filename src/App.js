2fc9b2a04e63a4c3c66afd4ff271d2d4e83230c2
Karel Smola <karelsmola21@gmail.com>
2023-04-16 23:12:25 +0200

    Reservation component added.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from "./components/Navigation";
 import Hero from "./components/Hero";
 import Gallery from "./components/Gallery";
 import Pricelist from "./components/Pricelist";
+import Reservation from "./components/Reservation";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
@@ -13,6 +14,7 @@ const App = () => {
       <Hero />
       <Gallery />
       <Pricelist />
+      <Reservation />
       <Contact />
       <Footer />
     </main>

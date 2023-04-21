77f83dad19008db91d320f33f221014e6c2a067c
Karel Smola <karelsmola21@gmail.com>
2023-04-21 22:24:57 +0200

    Mobile navigation is growing, but after some beers my brain is not so fresh. Good night.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,35 +1,47 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import Menu from "./UI/Icons/Menu";
 import Logo from "./UI/Icons/Logo";
 
 import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
+  const [mobileNav, setMobileNav] = useState(false);
+
+  const toggleMobileNav = () => {
+    setMobileNav((prevState) => !prevState);
+  };
+
+  const navClasses = mobileNav
+    ? `${classes.navigation} ${classes.visible}`
+    : `${classes.navigation}`;
+
   return (
-    <div className={classes.navigation}>
-      <div className={classes["logo-box"]}>
-        <Logo className={classes.logo} />
-      </div>
-      <div className={classes.test}>
+    <Fragment>
+      <button className={classes["mobile-nav"]} onClick={toggleMobileNav}>
         <Menu />
+      </button>
+      <div className={navClasses}>
+        <div className={classes["logo-box"]}>
+          <Logo className={classes.logo} />
+        </div>
+        <nav className={classes.nav}>
+          <ul className={classes["nav-list"]}>
+            <li className={classes["nav-item"]}>
+              <a href="#about">O nás</a>
+            </li>
+            <li className={classes["nav-item"]}>
+              <a href="#price">Ceník</a>
+            </li>
+            <li className={classes["nav-item"]} onClick={props.order}>
+              <a href="order">Objednat</a>
+            </li>
+            <li className={classes["nav-item"]}>
+              <a href="#contact">Kontakt</a>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav className={classes.nav}>
-        <ul className={classes["nav-list"]}>
-          <li className={classes["nav-item"]}>
-            <a href="#about">O nás</a>
-          </li>
-          <li className={classes["nav-item"]}>
-            <a href="#price">Ceník</a>
-          </li>
-          <li className={classes["nav-item"]} onClick={props.order}>
-            <a href="order">Objednat</a>
-          </li>
-          <li className={classes["nav-item"]}>
-            <a href="#contact">Kontakt</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    </Fragment>
   );
 };
 

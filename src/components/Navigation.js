2da026d63fd99bd9c7312252a7c0d729ca58fea6
Karel Smola <karelsmola21@gmail.com>
2023-04-22 11:06:58 +0200

    Mobile nav tuning

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,44 +1,49 @@
 import React, { Fragment, useState } from "react";
-import Menu from "./UI/Icons/Menu";
 import Logo from "./UI/Icons/Logo";
 
 import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
   const [mobileNav, setMobileNav] = useState(false);
+  const [crossMobileNav, setCrossMobileNav] = useState(false);
 
   const toggleMobileNav = () => {
     setMobileNav((prevState) => !prevState);
+    setCrossMobileNav((prevCross) => !prevCross);
   };
 
   const navClasses = mobileNav
-    ? `${classes.navigation} ${classes.visible}`
-    : `${classes.navigation}`;
+    ? `${classes.nav} ${classes.visible}`
+    : `${classes.nav}`;
+
+  const mobileNavClasses = crossMobileNav
+    ? `${classes["mobile-nav"]} ${classes.cross}`
+    : `${classes["mobile-nav"]}`;
 
   return (
     <Fragment>
-      <button className={classes["mobile-nav"]} onClick={toggleMobileNav}>
-        <Menu />
-      </button>
-      <div className={navClasses}>
+      <div className={classes.navigation}>
         <div className={classes["logo-box"]}>
           <Logo className={classes.logo} />
         </div>
-        <nav className={classes.nav}>
+        <nav className={navClasses}>
           <ul className={classes["nav-list"]}>
-            <li className={classes["nav-item"]}>
+            <li className={classes["nav-item"]} onClick={toggleMobileNav}>
               <a href="#about">O nás</a>
             </li>
-            <li className={classes["nav-item"]}>
+            <li className={classes["nav-item"]} onClick={toggleMobileNav}>
               <a href="#price">Ceník</a>
             </li>
             <li className={classes["nav-item"]} onClick={props.order}>
               <a href="order">Objednat</a>
             </li>
-            <li className={classes["nav-item"]}>
+            <li className={classes["nav-item"]} onClick={toggleMobileNav}>
               <a href="#contact">Kontakt</a>
             </li>
           </ul>
+          <button className={mobileNavClasses} onClick={toggleMobileNav}>
+            <div className={classes.menu} />
+          </button>
         </nav>
       </div>
     </Fragment>

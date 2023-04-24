dbd62d9a3402d3fd139af1d56a898ef89cd5c123
Karel Smola <karelsmola21@gmail.com>
2023-04-24 18:08:34 +0200

    Some changes in Service, PriceList, Hero and ReservForm

--- src/components/Pricelist.js
+++ src/components/Pricelist.js
@@ -63,61 +63,6 @@ const Pricelist = () => {
           />
         );
       })}
-
-      {/* <div className={classes["service-box"]}>
-        <h2 className={classes["service-title"]}>Střih vlasů</h2>
-        <button>Více</button>
-        <p className={classes["service-text"]}>
-          Střih vlasů, zaholení kontur, úprava obočí, mytí vlasů, masáž hlavy,
-          styling
-        </p>
-        <p className={classes["service-time"]}>45 - 60 min</p>
-        <p className={classes["service-price"]}>610 Kč</p>
-      </div>
-
-      <div className={classes["service-box"]}>
-        <h2 className={classes["service-title"]}>
-          Střih vlasů + holení / úprava vousů
-        </h2>
-        <p className={classes["service-text"]}>
-          Střih vlasů, zaholení kontur, úprava obočí, mytí vlasů, masáž hlavy,
-          styling + holení dohladka HOT TOWEL nebo zkrácení vousů se zaholením,
-          ošetření pokožky po holení
-        </p>
-        <p className={classes["service-time"]}>60 min</p>
-        <p className={classes["service-price"]}>940 Kč</p>
-      </div>
-
-      <div className={classes["service-box"]}>
-        <h2 className={classes["service-title"]}>Střih vlasů strojkem</h2>
-        <p className={classes["service-text"]}>
-          Střih vlasů pouze strojkem, zaholení kontur, úprava obočí, mytí vlasů,
-          masáž hlavy, styling
-        </p>
-        <p className={classes["service-time"]}>30 min</p>
-        <p className={classes["service-price"]}>400 Kč</p>
-      </div>
-
-      <div className={classes["service-box"]}>
-        <h2 className={classes["service-title"]}>
-          Chlapecký střih vlasů (7 - 12 let)
-        </h2>
-        <p className={classes["service-text"]}>
-          Střih vlasů, mytí vlasů, styling
-        </p>
-        <p className={classes["service-time"]}>30 min</p>
-        <p className={classes["service-price"]}>500 Kč</p>
-      </div>
-
-      <div className={classes["service-box"]}>
-        <h2 className={classes["service-title"]}>Holení / úprava vousů</h2>
-        <p className={classes["service-text"]}>
-          Holení dohladka HOT TOWEL nebo zkrácení vousů se zaholením, ošetření
-          pokožky po holení
-        </p>
-        <p className={classes["service-time"]}>30 min</p>
-        <p className={classes["service-price"]}>450 Kč</p>
-      </div> */}
     </div>
   );
 };
--- src/components/Service.js
+++ src/components/Service.js
@@ -3,7 +3,12 @@ import React, { useState } from "react";
 import classes from "./Service.module.css";
 
 const Service = ({ title, text, time, price }) => {
-  const [showMore, setShowMore] = useState(false);
+  const [showMore, setShowMore] = useState(true);
+
+  const watcher = window.matchMedia("(max-width: 500px)").matches;
+  if (watcher) {
+    console.log("watch");
+  }
 
   const toggleMore = () => {
     setShowMore((prevShow) => !prevShow);
@@ -12,7 +17,9 @@ const Service = ({ title, text, time, price }) => {
   return (
     <div className={classes["service-box"]}>
       <h2 className={classes["service-title"]}>{title}</h2>
-      <button onClick={toggleMore}>Více</button>
+      <button className={classes["service-btn"]} onClick={toggleMore}>
+        Více
+      </button>
       {showMore && (
         <div className={classes["more-box"]}>
           <p className={classes["service-text"]}>{text}</p>
